80a4a4b422f8bd90e83507845da1e68cfa0ecd13
doonee <doonee@outlook.com>
2022-11-18 13:35:25 +0900

    updated bookmarkModal

--- src/components/BookmarkModal.js
+++ src/components/BookmarkModal.js
@@ -1,219 +1,71 @@
+import React, { useState } from "react";
+import { Container, Row, Col, ButtonGroup } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 
 function BookmarkModal(props) {
   return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered>
+    <Modal {...props} centered>
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
+        <Modal.Title>북마크 관리</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Lorem LoremReprehenderit veniam ipsum aliqua cillum mollit duis
-          aliqua. Et eu ut consequat id tempor irure pariatur dolore. Incididunt
-          consequat sint veniam officia deserunt proident. In laboris do anim
-          veniam nulla do elit commodo laboris commodo dolore quis irure
-          commodo. Occaecat cillum consectetur cupidatat id. Consequat eiusmod
-          veniam ipsum tempor adipisicing consectetur c Lorem LoremReprehenderit
-          veniam ipsum aliqua cillum mollit duis aliqua. Et eu ut consequat id
-          tempor irure pariatur dolore. Incididunt consequat sint veniam officia
-          deserunt proident. In laboris do anim veniam nulla do elit commodo
-          laboris commodo dolore quis irure commodo. Occaecat cillum consectetur
-          cupidatat id. Consequat eiusmod veniam ipsum tempor adipisicing
-          consectetur cillum occaecat esse. Eu velit do culpa ullamco dolore
-          cillum dolore veniam laborum ea. Ipsum nostrud magna non dolor aliqua
-          mollit incididunt aute. Fugiat eu voluptate aute adipisicing ipsum do
-          laborum duis exercitation amet ut labore eu. Duis commodo velit
-          reprehenderit minim esse occaecat. Labore est mollit nulla et non
-          eiusmod nostrud adipisicing magna occaecat. Enim in consequat nostrud
-          in. Enim magna ad dolore sunt. Duis veniam ullamco ex adipisicing
-          exercitation nisi dolor qui quis. Quis amet dolore non exercitation
-          eiusmod consequat in. Anim magna aliqua anim cupidatat pariatur. Et
-          qui commodo eiusmod elit minim aliquip velit cillum voluptate est.
-          Consectetur reprehenderit qui cillum labore velit aliqua Lorem
-          proident culpa cillum nostrud cillum. Esse ipsum voluptate sunt duis
-          officia cupidatat cillum cillum ut velit dolore aliquip nostrud
-          eiusmod. Nisi pariatur veniam laboris est sunt tempor consectetur non
-          tempor cupidatat anim et deserunt deserunt. Nostrud in adipisicing
-          voluptate labore. Magna eiusmod qui excepteur eiusmod proident aliqua
-          esse. Cras mattis consectetur purus sit amet fermentum. Cras justo
-          odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus,
-          porta ac consectetur ac, vestibulum at eros.
-        </p>
+        <Form>
+          <Form.Group className="mb-3" controlId="bookmarkGroup">
+            <Form.Label>북마크 그룹</Form.Label>
+            <Form.Select>
+              <option value="1">그룹1</option>
+              <option value="2">그룹2</option>
+              <option value="2">그룹3</option>
+              <option value="3">:: 그룹 관리 ::</option>
+            </Form.Select>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="bookmarkCategory">
+            <Form.Label>북마크 카테고리</Form.Label>
+            <Form.Select>
+              <option value="1">카테고리1</option>
+              <option value="2">카테고리2</option>
+              <option value="2">카테고리3</option>
+              <option value="3">:: 카테고리 관리 ::</option>
+            </Form.Select>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="bookmarkUrl">
+            <Form.Label>북마크 URL</Form.Label>
+            <Form.Control type="url" placeholder="북마크 URL을 입력하세요." />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="bookmarkName">
+            <Form.Label>북마크 명</Form.Label>
+            <Form.Control type="text" placeholder="북마크 명을 입력하세요." />
+            <ButtonGroup className="mt-2">
+              <Form.Check inline label="중요" type="checkbox" id="11111" />
+              <Form.Check inline label="취소선" type="checkbox" id="22222" />
+            </ButtonGroup>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="bookmarkMemo">
+            <Form.Label>메모</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+        </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
+        <Container>
+          <Row>
+            <Col>
+              <Button variant="danger w-100" type="button">
+                삭제
+              </Button>
+            </Col>
+            <Col>
+              <Button variant="primary w-100" type="submit">
+                저장
+              </Button>
+            </Col>
+          </Row>
+        </Container>
       </Modal.Footer>
     </Modal>
-
-    // <Modal.Dialog>
-    //   <Modal.Header closeButton>
-    //     <Modal.Title>Modal title</Modal.Title>
-    //   </Modal.Header>
-
-    //   <Modal.Body>
-    //     <p>Modal body text goes here.</p>
-    //   </Modal.Body>
-
-    //   <Modal.Footer>
-    //     <Button variant="secondary">Close</Button>
-    //     <Button variant="primary">Save changes</Button>
-    //   </Modal.Footer>
-    // </Modal.Dialog>
   );
 }
 
 export default BookmarkModal;
-
-// const BookmarkModal = () => {
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     return false;
-//   };
-
-//   return (
-//     <div
-//       className="modal fade"
-//       id="newModal"
-//       tabIndex="-1"
-//       aria-labelledby="newModalLabel">
-//       <div className="modal-dialog modal-dialog-centered">
-//         <div className="modal-content modal-dialog-scrollable">
-//           <form onSubmit={handleSubmit}>
-//             <div className="modal-header">
-//               <h5 className="modal-title">북마크 관리</h5>
-//               <button
-//                 type="button"
-//                 className="btn-close"
-//                 data-bs-dismiss="modal"
-//                 aria-label="Close"></button>
-//             </div>
-//             <div className="modal-body">
-//               <div className="table-responsive">
-//                 <table
-//                   className="table table-striped"
-//                   style={{ tableLayout: "fixed" }}>
-//                   <thead>
-//                     <tr>
-//                       <td className="col-3 text-end">그룹</td>
-//                       <td className="col-9 text-start">
-//                         <select
-//                           className="form-select"
-//                           id="sel-new-modal-names">
-//                           <option value="1345">그룹1</option>
-//                           <option value="1345">그룹2</option>
-//                           <option value="1345">그룹3</option>
-//                           <option value="1345">그룹4</option>
-//                           <option value="1345">그룹5</option>
-//                         </select>
-//                       </td>
-//                     </tr>
-//                     <tr>
-//                       <td className="col-3 text-end">카테고리</td>
-//                       <td className="col-9 text-start">
-//                         <select
-//                           className="form-select"
-//                           id="sel-new-modal-names">
-//                           <option value="1345">카테고리1</option>
-//                           <option value="1345">카테고리2</option>
-//                           <option value="1345">카테고리3</option>
-//                           <option value="1345">카테고리4</option>
-//                           <option value="1345">카테고리5</option>
-//                         </select>
-//                       </td>
-//                     </tr>
-//                   </thead>
-//                   <tbody>
-//                     <tr>
-//                       <td className="col-3 text-end">북마크 URL</td>
-//                       <td className="col-9 text-start">
-//                         <input
-//                           type="text"
-//                           className="form-control"
-//                           id="txt-bookmark-url"
-//                         />
-//                       </td>
-//                     </tr>
-//                     <tr>
-//                       <td className="col-3 text-end">북마크명</td>
-//                       <td className="col-9 text-start">
-//                         <input
-//                           type="text"
-//                           className="form-control"
-//                           id="txt-bookmark-title"
-//                         />
-//                         <div
-//                           className="btn-group p-2"
-//                           role="group"
-//                           aria-label="Basic checkbox toggle button group">
-//                           <input
-//                             type="checkbox"
-//                             className="btn-check"
-//                             id="btncheck1"
-//                             autoComplete="off"
-//                           />
-//                           <label
-//                             className="btn btn-outline-primary"
-//                             htmlFor="btncheck1">
-//                             중요
-//                           </label>
-
-//                           <input
-//                             type="checkbox"
-//                             className="btn-check"
-//                             id="btncheck2"
-//                             autoComplete="off"
-//                           />
-//                           <label
-//                             className="btn btn-outline-primary"
-//                             htmlFor="btncheck2">
-//                             취소선
-//                           </label>
-//                         </div>
-//                       </td>
-//                     </tr>
-//                     <tr>
-//                       <td className="text-end">메모</td>
-//                       <td className="text-start">
-//                         <textarea
-//                           className="form-control"
-//                           id="txt-new-memo"></textarea>
-//                       </td>
-//                     </tr>
-//                   </tbody>
-//                 </table>
-//               </div>
-//             </div>
-//             <div className="modal-footer row">
-//               <div className="col">
-//                 <button
-//                   type="submit"
-//                   className="btn btn-danger w-100"
-//                   id="btn-new-modal-delete">
-//                   삭제
-//                 </button>
-//               </div>
-//               <div className="col">
-//                 <button
-//                   type="submit"
-//                   className="btn btn-primary w-100"
-//                   id="btn-new-modal-save">
-//                   저장
-//                 </button>
-//               </div>
-//             </div>
-//           </form>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default BookmarkModal;
--- src/components/Contents.js
+++ src/components/Contents.js
@@ -35,17 +35,32 @@ const Contents = () => {
   };
 
   var items = [
-    { id: 1, name: "My First Item" },
+    { id: 1, name: "1 My First Item" },
     {
       id: 2,
-      name: "Ipsum aute ipsum ullamco sint anim culpa aliqua est in dolore exercitation.",
+      name: "2 Ipsum aute ipsum ullamco sint anim culpa aliqua est in dolore exercitation.",
     },
     {
       id: 3,
-      name: "In nostrud sunt proident pariatur exercitation adipisicing. Commodo magna officia proident aute aute nisi laborum commodo laboris enim ex eu nisi exercitation. Esse laborum magna ipsum elit consequat commodo. Ea Lorem occaecat ea exercitation tempor nostrud ad adipisicing dolore. Ut id deserunt sint non esse. Est fugiat eu esse Lorem cillum et commodo esse cupidatat cupidatat sunt magna.",
+      name: "3 In nostrud sunt proident pariatur exercitation adipisicing. Commodo magna officia proident aute aute nisi laborum commodo laboris enim ex eu nisi exercitation. Esse laborum magna ipsum elit consequat commodo. Ea Lorem occaecat ea exercitation tempor nostrud ad adipisicing dolore. Ut id deserunt sint non esse. Est fugiat eu esse Lorem cillum et commodo esse cupidatat cupidatat sunt magna.",
     },
-    { id: 4, name: "Here is the Fourth" },
-    { id: 5, name: "High Five" },
+    { id: 4, name: "4 Here is the Fourth" },
+    { id: 5, name: "5 High Five" },
+    {
+      id: 6,
+      name: "6 Ex sit in nostrud sit eiusmod quis nisi sunt nostrud quis mollit occaecat.",
+    },
+    {
+      id: 7,
+      name: "7 Deserunt exercitation anim nostrud culpa occaecat deserunt non eiusmod et occaecat elit veniam.",
+    },
+    { id: 8, name: "8 888" },
+    { id: 9, name: "9 abcdefghijk" },
+    {
+      id: 10,
+      name: "10 Nisi cillum aliqua nulla aliquip tempor deserunt cillum sint.",
+    },
+    { id: 11, name: "11 11번째 카테고리" },
   ];
 
   items = items.map(function (item) {
@@ -53,7 +68,7 @@ const Contents = () => {
       <div key={item.id}>
         <h6>
           <a href="/bookmarks/2/1223" data-category="1223">
-            <strong>자주 찾는 페이지</strong>
+            <strong>{item.name}</strong>
           </a>
           &nbsp;
           <span>
@@ -74,7 +89,19 @@ const Contents = () => {
             data-bookmark-no="7108">
             deseru Lorem, ipsum dolor.nt. Culpa, vitae veritatis.
             <br />
-            <small>- Lorem ipsum dolor sit amet consect</small>
+            <small>- small text-muted small text-muted small text-muted</small>
+          </a>
+        </p>
+        <p>
+          <Icon.InfoCircle onClick={(e) => showBookmarkModal(e)} />
+          &nbsp;&nbsp;
+          <a
+            target="_blank"
+            href="https://music.youtube.com/"
+            data-bookmark-no="7108">
+            deseru Lorem, ipsum dolor.nt. Culpa, vitae veritatis.
+            <br />
+            <small class="text-muted">- small small small small</small>
           </a>
         </p>
         <p>
@@ -85,6 +112,8 @@ const Contents = () => {
             href="https://music.youtube.com/"
             data-bookmark-no="7108">
             <strong>Lorem ipsum dolor sit.</strong>
+            <br />
+            <small>- Lorem ipsum dolor sit ame7777777777</small>
           </a>
         </p>
         <p>
@@ -95,9 +124,9 @@ const Contents = () => {
             href="https://music.youtube.com/"
             data-bookmark-no="7108">
             <del>
-              Lorem, ipsum dolor.
+              Lorem, ipsum dolor.11111112222222
               <br />
-              <small>- Lorem ipsum dolor sit amet.</small>
+              <small>- Lorem ipsum dolor sit ame.</small>
             </del>
           </a>
         </p>
