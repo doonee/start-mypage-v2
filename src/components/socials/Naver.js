494192d066effbff0a010ff5a1ee6a2d41e54e50
doonee <doonee@outlook.com>
2023-01-04 11:02:21 +0900

    done naver login frontend

--- src/components/socials/Naver.js
+++ src/components/socials/Naver.js
@@ -1,6 +1,10 @@
 /*
 네이버 로그인 API (네아로) 의 모든 것 : 1부 (연동하기)
 https://velog.io/@rxxdo/%EB%A6%AC%EC%95%A1%ED%8A%B8%EB%A1%9C-%EB%84%A4%EC%9D%B4%EB%B2%84-%EC%86%8C%EC%85%9C-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EC%A0%81%EC%9A%A9%ED%95%98%EA%B8%B0-1%EB%B6%80-%EB%84%A4%EC%95%84%EB%A1%9C-%EC%A0%81%EC%9A%A9%ED%95%98%EA%B8%B0
+네이버 로그인 API (네아로) 의 모든 것 : 2부 (버튼 커스텀)
+https://velog.io/@rxxdo/%EB%84%A4%EC%9D%B4%EB%B2%84-%EB%A1%9C%EA%B7%B8%EC%9D%B8-API-%EB%84%A4%EC%95%84%EB%A1%9C-%EC%9D%98-%EB%AA%A8%EB%93%A0-%EA%B2%83-%EB%B2%84%ED%8A%BC-%EC%BB%A4%EC%8A%A4%ED%85%80-feat.-useRef
+[Javascript] Naver 로그인 버튼 커스텀하기
+https://minggu92.tistory.com/37
 */
 import React, { useEffect, useState } from 'react'
 import { useScript } from "../Hooks";
